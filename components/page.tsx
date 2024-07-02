f1ae34ddcb49617e0b7a8bc84a4bc416505ecb93
Kayla Lin <klin23@u.rochester.edu>
2024-07-02 10:11:13 -0400

    fix: dec

--- components/page.tsx
+++ components/page.tsx
@@ -3,7 +3,6 @@ import Appbar from '@/components/appbar'
 import BottomNav from '@/components/bottom-nav'
 import { Inter as FontSans } from 'next/font/google'
 import { cn } from '@/lib/utils'
-import { useRouter } from 'next/router'
 
 const fontSans = FontSans({
 	subsets: ['latin'],
@@ -16,8 +15,6 @@ interface Props {
 }
 
 const Page = ({ title, children }: Props) => {
-	const router = useRouter()
-	const { slug } = router.query
 	const ogImage = `https://www.yenny.app/images/opengraph.png'`
 
 	return (
@@ -26,27 +23,6 @@ const Page = ({ title, children }: Props) => {
 				<Head>
 					<title>Yenny | {title}</title>
 					<title>Yenny | Japanese Yen Converter</title>
-					<meta
-						name='description'
-						content='Yenny is a Japanese Yen (JPY) converter that can convert Japanese Yen to the US Dollar (USD), Euros (EUR), Great British Pound (GPB), and any other currency.'
-					/>
-					<meta property='og:url' content='https://www.yenny.app/' />
-					<meta property='og:type' content='website' />
-					<meta property='og:title' content='Yenny | Japanese Yen Converter' />
-					<meta
-						property='og:description'
-						content='Yenny is a Japanese Yen (JPY) converter that can convert Japanese Yen to the US Dollar (USD), Euros (EUR), Great British Pound (GPB), and any other currency.'
-					/>
-					<meta property='og:image' content={ogImage} />
-					<meta name='twitter:card' content='summary_large_image' />
-					<meta property='twitter:domain' content='yenny.app' />
-					<meta property='twitter:url' content='https://www.yenny.app/' />
-					<meta name='twitter:title' content='Yenny | Japanese Yen Converter' />
-					<meta
-						name='twitter:description'
-						content='Yenny is a Japanese Yen (JPY) converter that can convert Japanese Yen to the US Dollar (USD), Euros (EUR), Great British Pound (GPB), and any other currency.'
-					/>
-					<meta name='twitter:image' content={ogImage} />
 				</Head>
 			) : null}
 			<div className={cn('font-sans antialiased', fontSans.variable)}>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,6 +18,34 @@ export default function Document() {
 				<meta name='theme-color' content='#f4f4f5' />
 				<link rel='apple-touch-icon' href='/images/icon-maskable-512.png' />
 				<link rel='manifest' href='/manifest.json' />
+
+				<meta
+					name='description'
+					content='Yenny is a Japanese Yen (JPY) converter that can convert Japanese Yen to the US Dollar (USD), Euros (EUR), Great British Pound (GPB), and any other currency.'
+				/>
+				<meta property='og:url' content='https://www.yenny.app/' />
+				<meta property='og:type' content='website' />
+				<meta property='og:title' content='Yenny | Japanese Yen Converter' />
+				<meta
+					property='og:description'
+					content='Yenny is a Japanese Yen (JPY) converter that can convert Japanese Yen to the US Dollar (USD), Euros (EUR), Great British Pound (GPB), and any other currency.'
+				/>
+				<meta
+					property='og:image'
+					content={`https://www.yenny.app/images/opengraph.png'`}
+				/>
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta property='twitter:domain' content='yenny.app' />
+				<meta property='twitter:url' content='https://www.yenny.app/' />
+				<meta name='twitter:title' content='Yenny | Japanese Yen Converter' />
+				<meta
+					name='twitter:description'
+					content='Yenny is a Japanese Yen (JPY) converter that can convert Japanese Yen to the US Dollar (USD), Euros (EUR), Great British Pound (GPB), and any other currency.'
+				/>
+				<meta
+					name='twitter:image'
+					content={`https://www.yenny.app/images/opengraph.png'`}
+				/>
 			</Head>
 			<body>
 				<Main />
